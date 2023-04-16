da67a44900afad19383b44e2abdfd52a0ae2fa11
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-04-16 22:09:16 +0200

    Create moveAPI.js

---
+++ src/service/moveAPI.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = 'f55fd89359b4e389c455cb757e994b30';
+
+
+export const getTrendDay = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${API_KEY}`);
+  return response.data.results;
+};
+
+export const getSearchingByQuery = async query => {
+  const response = await axios.get(
+    `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+  );
+  return response.data.results;
+};
+
+export const fetchMovieById = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data;
+};
+
+export const fetchActors = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return response.data.cast;
+};
+
+export const fetchReviews = async movieId => {
+  const response = await axios.get(
+    `movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return response.data.results;
+};
+
+
