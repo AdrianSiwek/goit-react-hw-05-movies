e99bc2941e786b69c5dbf7a52968810f1624a226
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-04-16 21:43:25 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import style from './App.module.css'
+
+
+const App = () => {
+  return ( 
+    <div className="App">
+      Działa
     </div>
-  );
-};
+   );
+}
+ 
+export default App;
